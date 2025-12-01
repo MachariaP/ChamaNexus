631cbb254d3557be2400b6cc2e3ca65517dc3958
Phinehas <walburphinehas78@gmail.com>
2025-12-01 22:01:54 +0300

    Removed the build command

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-# Build script for Render deployment
-
+# Exit on error
 set -o errexit
 
-# Install Python dependencies
+# Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Run database migrations
-python manage.py migrate
